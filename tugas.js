3f04e7f5945370b3a9c885eaa386754c161a45f2
hndynisuci <hndynisuci4@gmail.com>
2025-01-14 21:28:17 -0800

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,20 @@
+let namaDepan = "Cahaya"
+let namaTengah = "Shifafitria"
+let namaBelakang = "Aji"
+
+let nama = " Cahaya Shifa Fitria Aji";
+let alamat = " kalinilam, kalimantan barat";
+let usia = " 15 tahun";
+let ttl = " jepara, 24,09,2009";
+let gender = " perempuan";
+let agama = " islam";
+let email = " fitriaaci13@gmail.com";
+let berat = " 43 kg";
+let tinggi = " 157 cm";
+let hobi = " menggambar";
+let cita = " dokter";
+let pekerjaan = " pelajar";
+let sekolah = " SMKN 1 SAYUNG";
+let jurusan = " X PPLG 1";
+
+document.write = ("nama saya adalah " + namaDepan + " " + namaTengah + " " + namaBelakang + " " + "saya tinggal di " + alamat +)
